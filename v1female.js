0f974498aad867b26b51d8871f29403230a793bb
jamalat <67556909+jamalat@users.noreply.github.com>
2020-12-01 12:48:22 +0200

    Update v1female.js

--- v1female.js
+++ v1female.js
@@ -92,15 +92,15 @@ define(['pipAPI'], function(APIconstructor) {
 
     API.addStimulusSets({
         defaultStim : [{css:{color:'black', 'font-size':'30px'}}],
-        Q1          : [{inherit:'defaultStim', media: 'حددي ما  نوع الشعور الذي اتاكي من  الجمله عندما تخيلتي الوضع الموصوف.اذا كان إيجابي اضغطي   1 واذا كان حيادي اضغط 2 واذا سلبي اضغط  3'}],
+        Q1          : [{inherit:'defaultStim', media: 'حددي ما  نوع الشعور الذي اتاكي من  الجمله عندما تخيلتي الوضع الموصوف ؟اذا كان إيجابي اضغطي   1 اذا كان حيادي اضغط 2 اذا سلبي اضغط  3'}],
         Q2          : [{inherit:'defaultStim', media: 'حددي من 1 حتى 9 ما هي شدة هذا الشعور ؟1 بلا شعور 9 شعور قوي جدا   '}],    
 	    Q3          : [{inherit:'defaultStim', media: 'حددي من 1 حتى 9 ما هو احتمال حصول ما ذكر بالجمله لكي ؟1 لا يوجد احتمال 9 احتمال قوي جدا  '}],
         Q4          : [{inherit:'defaultStim', media: 'حددي من 1 حتى 9 ما مدى سرعة قفز الوضع الموصوف الى مخيلتك فور قراءتها ؟1 بطئ جدا 9 سريع جدا '}],      
-	    Q5          : [{inherit:'defaultStim', media: 'حددي من 1 حتى 9 مدى كون الوضع الموصوف في الجمله حي ومفصل  ؟1 ليس حي ولا مفصل 9 حي ومفصل جدا '}],
-        Q6          : [{inherit:'defaultStim', media: 'حددي من 1 حتى 9 كم استهلكتي من الجهد حتى تتوقفي عن التفكير بالوضع الموصوف بالجمله  ؟1 بلاا جهد9 جهد كبير  '}],    
-	    Q7          : [{inherit:'defaultStim', media: '  حددي من 1 حتى 9 كم كان الوضع الموصوف في الحمله مرتبط بطموحاتكي ؟1 غير مرتبط بتاتا9 مرتبط جدا  '}],
-        Q8          : [{inherit:'defaultStim', media: '  حددي من 1 حتى 9 كم كان الوضع الموصوف في الحمله مرتبط باحتياجاتكي ؟1 غير مرتبط بتاتا9 مرتبط جدا  '}], 
-	    Q9          : [{inherit:'defaultStim', media: ' حددي من 1 حتى 9 كم كان الوضع الموصوف في الجمله مرتبط بمخاوفكي؟1 غير مرتبط بتاتا9مرتبط جدا  '}]   
+	    Q5          : [{inherit:'defaultStim', media: 'حددي من 1 حتى 9 مدى كون الوضع الموصوف في الجملة حي ومفصل  ؟1 ليس حي ولا مفصل 9 حي ومفصل جدا '}],
+        Q6          : [{inherit:'defaultStim', media: 'حددي من 1 حتى 9 كم استهلكتي من جهد حتى تتوقفي عن التفكير بالوضع الموصوف بالجملة  ؟1 بلا جهد 9  جهد كبير  '}],    
+	    Q7          : [{inherit:'defaultStim', media: '  حددي من 1 حتى 9 كم كان الوضع الموصوف في الجملة مرتبط بطموحاتكي ؟1 غير مرتبط بتاتا9 مرتبط جدا  '}],
+        Q8          : [{inherit:'defaultStim', media: '  حددي من 1 حتى 9 كم كان الوضع الموصوف في الحملة مرتبط باحتياجاتكي ؟1 غير مرتبط بتاتا9 مرتبط جدا  '}], 
+	    Q9          : [{inherit:'defaultStim', media: ' حددي من 1 حتى 9 كم كان الوضع الموصوف في الجملة مرتبط بمخاوفكي؟1 غير مرتبط بتاتا9مرتبط جدا  '}]   
 	    
     });
 
@@ -494,7 +494,7 @@ define(['pipAPI'], function(APIconstructor) {
         {inherit: 'stimulus_trial', data: {text: 'انا انعطف لليمين اعبر من خلف الحديقه نحو البيت   ', sid: 146}},
 	{inherit: 'stimulus_trial', data: {text: 'أقوم بشغيل الجهاز بعد ان قرات التعليمات حول استخدامه ', sid: 147}},
         {inherit: 'stimulus_trial', data: {text: 'أقوم بوضع اشاره الى اين وصلت في الكتاب واغلقه ', sid: 148}},
-        {inherit: 'stimulus_trial', data: {text: 'انا ادخل الى الصالون واعبر منه الى الشرفه الاماميه  ', sid: 159}},
+        {inherit: 'stimulus_trial', data: {text: 'انا ادخل الى الصالون واعبر منه الى الشرفه الاماميه  ', sid: 149}},
         {inherit: 'stimulus_trial', data: {text: 'انا اضع اللاصقات على الكتب وارتبهم على رف المكتبه   ', sid:150}},
         {inherit: 'stimulus_trial', data: {text: 'انا افتح خزانة المطبخ لكي اتناول ملعقة طعام وصحن    ', sid: 151}},
         {inherit: 'stimulus_trial', data: {text: 'انا اطفا الضوء في الغرفه قبل ان أقوم باغلاقها   ', sid: 152}},
@@ -513,15 +513,15 @@ define(['pipAPI'], function(APIconstructor) {
         {inherit: 'stimulus_trial', data: {text: 'انا اصل الى بيت والداي  واجد مكان البيت انقاذا    ', sid: 218}},
         {inherit: 'stimulus_trial', data: {text: 'من خلال ثقب في الجدار يدخل للغرفه عشرات الجرذان ', sid: 219}},
         {inherit: 'stimulus_trial', data: {text: ' شفتاي  ملصقتان بالدبق وانا لا اقدر على فتح فمي       ', sid: 220}},
-        {inherit: 'stimulus_trial', data: {text: 'أنا اغمض عيناي وادخل في لعبة كمبيوتر وأقوم بربحها  ', sid: 248}},
-        {inherit: 'stimulus_trial', data: {text: 'انا أطير بسرعة في السماء مثل سوبرمان واتجاوز الغيوم  ', sid: 249}},
-        {inherit: 'stimulus_trial', data: {text: 'أركز بشدة وأتمكن من رفع كتاب بالهواء بقوة التفكير  ', sid: 250}},
-        {inherit: 'stimulus_trial', data: {text: '  ينتظرني خبير تجميل في المنزل ليفعل كل شيء لأجلي  ', sid: 251}},
-        {inherit: 'stimulus_trial', data: {text: ' انا أقوم بتكسير سيارة اكثر شخص اكرهه في العالم     ', sid: 252}},
-	{inherit: 'stimulus_trial', data: {text: 'يغمى علي وعند استيقاظي اكتشف انني استطيع قراءة  الأفكار   ', sid: 253}},
-        {inherit: 'stimulus_trial', data: {text: '  نمت لي  أجنحة ضخمة وانا الان أطير في السماء  ', sid: 254}},
-        {inherit: 'stimulus_trial', data: {text: ' يوجد معي عصا سحريه تقوم بتلبية كل  ما اريد     ', sid: 255}},
-        {inherit: 'stimulus_trial', data: {text: ' انا افوز بمنافسة ملكة جمال العالم لعام الفبن وعشرين      ', sid: 282}}
+        {inherit: 'stimulus_trial', data: {text: 'أنا اغمض عيناي وادخل في لعبة كمبيوتر وأقوم بربحها  ', sid: 247}},
+        {inherit: 'stimulus_trial', data: {text: 'انا أطير بسرعة في السماء مثل سوبرمان واتجاوز الغيوم  ', sid: 248}},
+        {inherit: 'stimulus_trial', data: {text: 'أركز بشدة وأتمكن من رفع كتاب بالهواء بقوة التفكير  ', sid: 249}},
+        {inherit: 'stimulus_trial', data: {text: '  ينتظرني خبير تجميل في المنزل ليفعل كل شيء لأجلي  ', sid: 250}},
+        {inherit: 'stimulus_trial', data: {text: ' انا أقوم بتكسير سيارة اكثر شخص اكرهه في العالم     ', sid: 251}},
+	{inherit: 'stimulus_trial', data: {text: 'يغمى علي وعند استيقاظي اكتشف انني استطيع قراءة  الأفكار   ', sid: 252}},
+        {inherit: 'stimulus_trial', data: {text: '  نمت لي  أجنحة ضخمة وانا الان أطير في السماء  ', sid: 253}},
+        {inherit: 'stimulus_trial', data: {text: ' يوجد معي عصا سحريه تقوم بتلبية كل  ما اريد     ', sid: 254}},
+        {inherit: 'stimulus_trial', data: {text: ' انا افوز بمنافسة ملكة جمال العالم لعام الفبن وعشرين      ', sid: 281}}
     ]);
    
     /***********************************************
